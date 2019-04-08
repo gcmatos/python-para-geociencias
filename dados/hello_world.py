fe777987cf17b01bc90693404cb486154ee7a298
Gabriel Matos <gcmatos@gmail.com>
2019-04-08 19:52:45 -0300

    Upload files to data dir

--- dados/hello_world.py
+++
@@ -1,6 +0,0 @@
-# Curso de Python para Geociências
-# Data: 2018-10-01
-# Exemplo: Imprimir a frase "Hello, World!"
-# Autor: Gabriel
-
-print("Hello, World!")
--- dados/test.py
+++
@@ -1,6 +0,0 @@
-# Curso de Python para Geociências
-# Data: 2018-10-01
-# Exemplo: Imprimir a versão Python
-import sys
-
-print("Python: ", sys.version)
